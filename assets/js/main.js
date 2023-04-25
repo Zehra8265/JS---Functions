996ca6b1956edaf51a6b51ad26696f39e7be48c5
Zehra8265 <8265RCL@code.edu.az>
2023-04-25 22:23:44 +0300

    task added

---
+++ assets/js/main.js
@@ -0,0 +1,90 @@
+ // 1 eded verilir. Eger o eded 50den kicikdirse o edede qeder 3-e bolunen, 50-100 araligindadirsa o edede qeder 5e bolunen, 100den boyukdurse o edede qeder 8 e bolunen ededlerin sayini ekrana cixardan bir alqoritm yazin.
+
+
+// function FindNumbers(number){
+
+//     var count = 0;
+
+//     if(number > 0 && number < 50){
+//         for(let i = 1; i < number; i++){
+//             if(i % 3 === 0){
+//                 count++;
+//             }
+//         }
+//     }
+//     else if(number > 50 && number < 100){
+//         for(let i = 1; i < number; i++){
+//             if(i % 5 === 0){
+//                 count++;
+//             }
+//         }
+//     }
+//     else {
+//         for(let i = 1; i < number; i++){
+//             if(i % 8 === 0){
+//                 count++;
+//             }
+//         }
+//     }
+//     return count;
+// }
+
+
+// alert(FindNumbers(+prompt("Eded daxil edin ")));
+
+
+
+
+
+
+
+// Toplama çıxma, toplama, vurma, bölməyə uyğun funksiya yaradirsiz 2 eded ve 1 operator('+','-','*','/') qebul edir. Qebul etdiyi operatora uygun a və b ədədləri üzərində hesablama aparib alinan deyeri qaytarır.
+// var number = Number(prompt("Birinci ededi daxil edin"));
+// var oper = prompt("Operatoru daxil edin");
+// var number2 = Number(prompt("Ikinci ededi daxil edin"));
+
+
+
+// switch (oper) {
+//     case "+":
+//         alert(num1 + num2);
+//         break;
+//     case "-":
+//         alert(num1 - num2);
+//         break;
+//     case "/":
+//         alert(num1 / num2);
+//         break;
+//     case "*":
+//         alert(num1 * num2)
+//         break;
+// }
+
+// Methodlar yaradirsiz. Methoda ad gonderende adi ekrana cixardir(Sabir), ad ve soyad gonderende soyadi adi ekrana cixardir(Guliyev Sabir), ad,soyad ve ata adı göndərildikdə adin baş hərfi nöqtə soyad ata adi ekrana cixardirsiz(S.Guliyev Mehti). Methodlarin adi eyni olmalıdır.
+
+
+//     var std={
+//         name:"Zəhra",
+//         surname:"Mehdizadə",
+//         farthername:"Mehdi"
+
+//     }
+// console.log(std.name);
+
+// var std1={
+//     name:"Zehra",
+//     surname:"Mehdizade",
+//     fathername:"Mehdi"
+
+
+
+// }
+// console.log(std1.surname +" "+ std1.name);
+// var std2={
+
+//     name:"Zehra",
+//     surname:"Mehdizade",
+//     fathername:"Mehdi"
+
+// }
+// console.log(std2.name[0]+ "."+std2.surname +" " + std2.fathername);
